e20b63edb32ed77d498e0e184bb2eb72e0aa489a
Intelwi <mlk04@wp.pl>
2021-09-17 11:00:11 +0200

    Implemented interface for saving game performance and fetching data

--- common/src/migrave_performance_tracking/performance_tracking.py
+++ common/src/migrave_performance_tracking/performance_tracking.py
@@ -1,14 +1,29 @@
 """Module for functionalities taking care of game performance tracking
 """
 
-import numpy as np
-
+from typing import Dict
+import rospy
 
 class RobotPerformanceTracking(object):
-    """Main component for executing actions
+    """Main component for tracking the child performance
     """
     def __init__(self):
         pass
 
-    def send_action(self, state):
+    def save_performance_record(self, performance_record: Dict) -> None:
+        'Function to save single performance record in the database'
+        rospy.loginfo('Saved performance record \n :{}'.format(performance_record))
         pass
+
+    def get_latest_performance_record(self) -> Dict:
+        'Function to fetch latest performance record'
+        return {}
+
+    def get_performance(self, child_name: str='', game_id: int=-1) -> Dict:
+        """
+        Function to fetch performannce of the child in the given type of game.
+        If child_name is not provided, overall performance for all children in the given game is fetched.
+        If game_id is not provided, overall performance in all games for a given child is fetched.
+        If non of the parameters are provided, performance for all children in all types of games is fetched. 
+        """
+        return {}
--- ros/src/migrave_performance_tracking_wrapper/performance_tracking_wrapper.py
+++ ros/src/migrave_performance_tracking_wrapper/performance_tracking_wrapper.py
@@ -1,14 +1,38 @@
 import rospy
 
 from migrave_performance_tracking.performance_tracking import RobotPerformanceTracking
-from migrave_ros_msgs.msg import GamePerformance
+from migrave_ros_msgs.msg import GamePerformance, GameActivity
 
 class PerformanceTrackingWrapper(object):
     def __init__(self):
-        pass
+        self.current_performance = {}
+        self.game_performance_tracker = RobotPerformanceTracking()
+        self.can_receive_new_feedback = True
 
-    def act(self) -> None:
-        rospy.loginfo('Tracking the game performance.')
+        game_performance_topic = rospy.get_param('~performance_topic', '/migrave/game_performance')
+        # Should be ros action in the future
+        self.action_sub = rospy.Subscriber(game_performance_topic,
+                                          GamePerformance,
+                                          self.game_performance_cb)
+        self.action_pub = rospy.Publisher(game_performance_topic, GamePerformance, queue_size=1)
 
-    def robot_action_cb(self, robot_performance_msg: GamePerformance) -> None:
-        pass
+    def game_performance_cb(self, performance_msg: GamePerformance) -> None:
+        if self.can_receive_new_feedback:
+            self.can_receive_new_feedback = False
+
+            self.current_performance['time'] = {'secs': performance_msg.stamp.secs, 'nsecs': performance_msg.stamp.nsecs}
+            self.current_performance['person'] = {'name': performance_msg.person.name,
+                                                'age': performance_msg.person.age,
+                                                'gender': performance_msg.person.gender,
+                                                'mother_tongue': performance_msg.person.mother_tongue}
+            self.current_performance['game_activity'] = {'game_id': performance_msg.game_activity.game_id,
+                                                        'game_activity_id': performance_msg.game_activity.game_activity_id,
+                                                        'difficulty_level': performance_msg.game_activity.difficulty_level}
+            self.current_performance['answer_correctness'] = performance_msg.answer_correctness
+
+            self.game_performance_tracker.save_performance_record(self.current_performance)
+            
+            rospy.sleep(1)
+            
+            self.can_receive_new_feedback = True
+            
